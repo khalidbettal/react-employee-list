ad35ad08ce0909816ff2f20c3c9635ec9accb5b9
khalidbettal <khalidbettal@gmail.com>
2023-09-16 22:51:35 +0000

    the first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import Update from './components/Update';
+
+
+
+function Employee(props){
+  return (
+<div className="min-w-[350px] max-w-[350px] py-8 px-8 max-w-sm m-2 bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+  <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={props.image} alt="Woman's Face" />
+  <div className="text-center space-y-2 sm:text-left">
+    <div className="space-y-0.5">
+      <p className="text-lg text-black font-semibold">
+        {props.name}
+      </p>
+      <p className="text-slate-500 font-medium">
+        {props.role}
+      </p>
+    </div>
+    <div >
+    <Update id={props.id} name={props.name} role={props.role} updateEmployee={props.updateEmployee}/>
+    </div>
+  </div>
+</div>
+  );
+  
+}
+
 
 function App() {
+
+  const [employees, setEmployees] = useState([
+    {
+      id: 1,
+      name: 'Erin Lindford',
+      role: 'Product Engineer',
+      image: 'https://gravatar.com/avatar/4b570bca28a60319dd506aaa48c2be7d?s=400&d=robohash&r=x'
+    },
+    {
+      id: 2,
+      name: 'jane Cooper',
+      role: 'developer',
+      image: 'https://gravatar.com/avatar/cdfc299c71ca8cbc8abea35972086f47?s=400&d=robohash&r=x'
+    },
+    {
+      id: 3,
+      name: 'cody fisher',
+      role: 'designer',
+      image: 'https://gravatar.com/avatar/030ff15988074841a47e990da7aea363?s=400&d=robohash&r=x'
+    },
+    {
+      id: 4,
+      name: 'maryem bettal',
+      role: 'animal lover',
+      image: 'https://gravatar.com/avatar/87ffd16399acc2ef56f795402ee7f9e6?s=400&d=robohash&r=x'
+    },
+    {
+      id: 5,
+      name: 'kristin watson',
+      role: 'react developer',
+      image: 'https://gravatar.com/avatar/8c25cc2d6487266a1b2683f2e4856498?s=400&d=robohash&r=x'
+    },
+    {
+      id: 6,
+      name: 'alexandra',
+      role: 'ios developer',
+      image: 'https://gravatar.com/avatar/fab6d8a498a3538278deffcb1fa665ab?s=400&d=robohash&r=x'
+    },
+    {
+      id: 7,
+      name: 'isabella',
+      role: 'front end developer',
+      image: 'https://gravatar.com/avatar/a582601c3c4bbc9ce334b5f2ecacec86?s=400&d=robohash&r=x'
+    },
+    {
+      id: 8,
+      name: 'quinn flynn',
+      role: 'software engineer',
+      image: 'https://gravatar.com/avatar/0adc8466f6d11e7ddaae8ca40e38e24b?s=400&d=robohash&r=x'
+    },
+    {
+      id: 9,
+      name: 'olivia jade',
+      role: 'ui/ux designer',
+      image: 'https://gravatar.com/avatar/b520527cdec0034ad5a32bedb54a3bad?s=400&d=robohash&r=x'
+    },
+
+  ]);
+
+  function updateEmployee(id, newName, newRole){
+    const updatedEmployees = employees.map((employee)=>{
+      if(id === employee.id){
+        return {...employee, name:newName, role:newRole};
+      }
+      return employee ;
+  
+    });
+    setEmployees(updatedEmployees)
+  
+  } 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-wrap justify-center sm:m-32 '>
+      {employees.map((employee) => (
+        <Employee 
+        key={employee.id}
+        id={employee.id}
+        name={employee.name} 
+        role={employee.role} 
+        image={employee.image} 
+        updateEmployee={updateEmployee}/>
+      ))}
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/Update.js
@@ -0,0 +1,79 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+
+import Modal from 'react-bootstrap/Modal';
+
+
+function Update(props) {
+  const [show, setShow] = useState(false);
+  const [name, setName] = useState(props.name);
+    const [role, setRole] = useState(props.role);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+        <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200
+         hover:text-white hover:bg-purple-600 hover:border-transparent 
+         focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+         onClick={handleShow}>Update</button>
+     
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+    
+          <form className="w-full max-w-sm" id='Update' onSubmit={
+            (e)=>{
+              e.preventDefault();
+              console.log('hello error');
+              console.log(props.id, name, role);
+              props.updateEmployee(props.id, name, role);
+
+            }
+          }>
+  <div className="md:flex md:items-center mb-6">
+    <div className="md:w-1/3">
+      <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+        Full Name
+      </label>
+    </div>
+    <div className="md:w-2/3">
+      <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value={name} 
+      onChange={
+        (e)=>{setName(e.target.value );
+        }} />
+    </div>
+  </div>
+  <div className="md:flex md:items-center mb-6">
+    <div className="md:w-1/3">
+      <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+        Role
+      </label>
+    </div>
+    <div className="md:w-2/3">
+      <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value={role}
+      onChange={
+        (e)=>{ setRole(e.target.value );
+        }}/>
+    </div>
+  </div>
+</form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <button className="px-6 py-2.5  text-sm text-purple-600 font-bold rounded-lg border border-purple-200
+         hover:text-white hover:bg-purple-600 hover:border-transparent 
+         focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+         form='Update' onClick={handleClose} >Update</button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default Update;
